ecd9d72256d978389720dcaf3dc0d9d05391c84f
Piero Giusti <pierophp@gmail.com>
2019-01-30 23:02:00 +0800

    Link wol

---
+++ api/app/core/sites/jw/wol.ts
@@ -0,0 +1,83 @@
+interface LanguageInterface {
+  [key: string]: {
+    contentLocale: string;
+    contentRsconf: string;
+    contentLib: string;
+  };
+}
+
+export class Wol {
+  protected getParts(): LanguageInterface {
+    const response = {
+      'cmn-hans': {
+        contentLocale: 'cmn-Hans',
+        contentRsconf: 'r23',
+        contentLib: 'lp-chs-rb',
+      },
+      'cmn-hant': {
+        contentLocale: 'cmn-Hant',
+        contentRsconf: 'r24',
+        contentLib: 'lp-ch',
+      },
+      de: {
+        contentLocale: 'de',
+        contentRsconf: 'r10',
+        contentLib: 'lp-x',
+      },
+      en: {
+        contentLocale: 'en',
+        contentRsconf: 'r1',
+        contentLib: 'lp-e',
+      },
+      es: {
+        contentLocale: 'es',
+        contentRsconf: 'r4',
+        contentLib: 'lp-s',
+      },
+      fr: {
+        contentLocale: 'fr',
+        contentRsconf: 'r30',
+        contentLib: 'lp-f',
+      },
+      it: {
+        contentLocale: 'it',
+        contentRsconf: 'r6',
+        contentLib: 'lp-i',
+      },
+      ja: {
+        contentLocale: 'ja',
+        contentRsconf: 'r7',
+        contentLib: 'lp-j',
+      },
+      ko: {
+        contentLocale: 'ko',
+        contentRsconf: 'r8',
+        contentLib: 'lp-ko',
+      },
+      pt: {
+        contentLocale: 'pt',
+        contentRsconf: 'r5',
+        contentLib: 'lp-t',
+      },
+    };
+
+    return response;
+  }
+
+  public changeUrlLanguage(url: string, language: string): string {
+    const parts = this.getParts();
+    if (!parts[language]) {
+      throw new Error('Language not found');
+    }
+
+    const languageParts = parts[language];
+
+    const urlParts = url.split('/');
+
+    urlParts[3] = languageParts.contentLocale;
+    urlParts[6] = languageParts.contentRsconf;
+    urlParts[7] = languageParts.contentLib;
+
+    return urlParts.join('/');
+  }
+}
