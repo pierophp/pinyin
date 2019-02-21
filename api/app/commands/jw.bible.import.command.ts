b8ca6a5060092212bcdabe00b441bb38786fd7b7
Piero Giusti <pierophp@gmail.com>
2019-02-21 18:04:26 +0800

    Fix Bible Import

--- api/app/commands/jw.bible.import.command.ts
+++ api/app/commands/jw.bible.import.command.ts
@@ -8,6 +8,14 @@ export class JwBibleImportCommand implements CommandModule {
   public async handler(argv: Arguments) {
     const bibleImporter = new BibleImporter();
     await bibleImporter.import();
+
+    // PARTIAL
+    // await bibleImporter.parseChapter(
+    //   '/cmn-Hans/wol/b/r23/lp-chs-rb/bi12/CHS/2001/1/15#study=discover',
+    //   0, // Genesis
+    //   '15', // Chapter
+    // );
+
     process.exit();
   }
 }
