1a70bcb1fd6fd8e0a6ea39677d376b3e8dc08439
Piero Giusti <pierophp@gmail.com>
2022-06-26 11:28:09 -0300

    Fix deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd api/
-[[ $TRAVIS_BRANCH = "master" ]] && DEPLOY_ENV="production" || DEPLOY_ENV="staging"
+[[ $GITHUB_REF_NAME = "master" ]] && DEPLOY_ENV="production" || DEPLOY_ENV="staging"
 echo "Deploy Starting: $DEPLOY_ENV"
 export SSH_KEY="/home/travis/.ssh/id_rsa"
 git checkout -- yarn.lock
