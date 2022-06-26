2d636f12816e806e49bd256313b50d96141f8460
Piero Giusti <pierophp@gmail.com>
2022-06-26 11:39:41 -0300

    Force deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,7 @@
 cd api/
 [[ $GITHUB_REF_NAME = "master" ]] && DEPLOY_ENV="production" || DEPLOY_ENV="staging"
 echo "Deploy Starting: $DEPLOY_ENV"
-export SSH_KEY="/home/runner/work/pinyin/pinyin/.ssh/id_rsa"
+export SSH_KEY="/home/runner//.ssh/id_rsa"
 git checkout -- yarn.lock
 export CMD="pm2 deploy ecosystem.config.js $DEPLOY_ENV --force"
 $CMD
