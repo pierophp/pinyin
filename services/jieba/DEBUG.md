7e487087156a528d0a99bb9f50f1e0556aa22afd
Piero Giusti <pierophp@gmail.com>
2022-07-08 15:57:02 -0300

    Remove folder

---
+++ services/jieba/DEBUG.md
@@ -0,0 +1,38 @@
+# Debug AWS lambda
+
+Add this to the function:
+
+```typescript
+import { exec } from 'node:child_process';
+
+const cmds = [
+  `ls -la /var/task`,
+  `ls -la /var/task/services`,
+  `ls -la /var/task/services/jieba`,
+  `ls -la /var/task/services/jieba/netlify`,
+  `ls -la /var/task/services/jieba/netlify/functions`,
+];
+
+let response = '';
+
+for (const cmd of cmds) {
+  response += `\n\n${cmd}\n`;
+  response += await new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (error) {
+        return reject(error);
+      }
+
+      resolve(stdout);
+    });
+  });
+}
+
+response += `\n\nENV:\n`;
+response += JSON.stringify(process.env);
+
+return {
+  statusCode: 200,
+  body: response,
+};
+```
--- services/jieba/netlify/functions/separateWords.ts
+++ services/jieba/netlify/functions/separateWords.ts
@@ -1,43 +1,10 @@
 import { Handler } from '@netlify/functions';
 import { separateChineseWords } from 'separate-chinese-words';
-import { exec } from 'node:child_process';
 
 const handler: Handler = async (event, context) => {
   const dictPath = process.env.NETLIFY_DEV
     ? `${process.env.PWD}/node_modules/separate-chinese-words/dict`
     : `${process.env.PWD}/services/jieba/dict`;
-
-  // `ls -la /var/task/services/jieba/netlify/functions/separateWords.js`,
-  // `cat /var/task/services/jieba/netlify/functions/separateWords.js`,
-  // const cmds = [
-  //   `ls -la /var/task`,
-  //   `ls -la /var/task/services`,
-  //   `ls -la /var/task/services/jieba`,
-  //   `ls -la /var/task/services/jieba/netlify`,
-  //   `ls -la /var/task/services/jieba/netlify/functions`,
-  // ];
-
-  // let response = '';
-
-  // for (const cmd of cmds) {
-  //   response += `\n\n${cmd}\n`;
-  //   response += await new Promise((resolve, reject) => {
-  //     exec(cmd, (error, stdout, stderr) => {
-  //       if (error) {
-  //         return reject(error);
-  //       }
-
-  //       resolve(stdout);
-  //     });
-  //   });
-  // }
-
-  // const response = JSON.stringify(process.env);
-
-  // return {
-  //   statusCode: 200,
-  //   body: response,
-  // };
 
   const body = event.body ? JSON.parse(event.body) : null;
 
