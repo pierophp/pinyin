98c12cb4dcefac321723acc5ab3732b9da653994
Piero Giusti <pierophp@gmail.com>
2022-07-08 14:00:08 -0300

    Fix deploy

--- services/jieba/netlify/functions/separateWords.ts
+++ services/jieba/netlify/functions/separateWords.ts
@@ -29,11 +29,9 @@ const handler: Handler = async (event, context) => {
 
   const response = separateChineseWords(body.text, {
     dictPath:
-      process.env.LAMBDA_RUNTIME_DIR +
-      '/node_modules/separate-chinese-words/dict/jieba.dict.utf8',
+      process.env.NODE_PATH + '/separate-chinese-words/dict/jieba.dict.utf8',
     userDictPath:
-      process.env.LAMBDA_RUNTIME_DIR +
-      '/node_modules/separate-chinese-words/dict/user.dict.utf8',
+      process.env.NODE_PATH + '/separate-chinese-words/dict/user.dict.utf8',
   }).filter((item) => {
     return item
       .replaceAll(String.fromCharCode(160), '') // Convert NO-BREAK SPACE to Space
