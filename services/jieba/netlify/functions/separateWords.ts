74955bb85501e6db43240db6f3d5c87a9f6736b3
Piero Giusti <pierophp@gmail.com>
2022-07-08 15:51:42 -0300

    Fix Path

--- services/jieba/netlify/functions/separateWords.ts
+++ services/jieba/netlify/functions/separateWords.ts
@@ -4,8 +4,8 @@ import { exec } from 'node:child_process';
 
 const handler: Handler = async (event, context) => {
   const dictPath = process.env.NETLIFY_DEV
-    ? `${process.env.PWD}/node_modules/separate-chinese-words/dict/`
-    : `${process.env.PWD}/services/jieba/node_modules`;
+    ? `${process.env.PWD}/node_modules/separate-chinese-words/dict`
+    : `${process.env.PWD}/services/jieba/dict`;
 
   // `ls -la /var/task/services/jieba/netlify/functions/separateWords.js`,
   // `cat /var/task/services/jieba/netlify/functions/separateWords.js`,
@@ -32,45 +32,44 @@ const handler: Handler = async (event, context) => {
   //   });
   // }
 
-  const response = JSON.stringify(process.env);
-
-  return {
-    statusCode: 200,
-    body: response,
-  };
-
-  // const body = event.body ? JSON.parse(event.body) : null;
-
-  // if (!body?.text) {
-  //   return {
-  //     statusCode: 400,
-  //     body: JSON.stringify({ message: 'Text is required' }),
-  //   };
-  // }
-
-  // const response = separateChineseWords(body.text, {
-  //   dictPath: nodeModulesPath + '/separate-chinese-words/dict/jieba.dict.utf8',
-  //   userDictPath:
-  //     nodeModulesPath + '/separate-chinese-words/dict/user.dict.utf8',
-  // }).filter((item) => {
-  //   return item
-  //     .replaceAll(String.fromCharCode(160), '') // Convert NO-BREAK SPACE to Space
-  //     .replaceAll(String.fromCharCode(8201), '') // Convert THIN SPACE to Space
-  //     .replaceAll(String.fromCharCode(8203), '') // Zero Width Space
-  //     .replaceAll(String.fromCharCode(8206), '') // Left-To-Right Mark
-  //     .replaceAll(String.fromCharCode(8234), '') // Left-To-Right Embedding
-  //     .trim();
-  // });
+  // const response = JSON.stringify(process.env);
 
   // return {
   //   statusCode: 200,
-  //   body: JSON.stringify({
-  //     words: response,
-  //   }),
-  //   headers: {
-  //     'content-type': 'application/json',
-  //   },
+  //   body: response,
   // };
+
+  const body = event.body ? JSON.parse(event.body) : null;
+
+  if (!body?.text) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Text is required' }),
+    };
+  }
+
+  const response = separateChineseWords(body.text, {
+    dictPath: dictPath + '/jieba.dict.utf8',
+    userDictPath: dictPath + '/user.dict.utf8',
+  }).filter((item) => {
+    return item
+      .replaceAll(String.fromCharCode(160), '') // Convert NO-BREAK SPACE to Space
+      .replaceAll(String.fromCharCode(8201), '') // Convert THIN SPACE to Space
+      .replaceAll(String.fromCharCode(8203), '') // Zero Width Space
+      .replaceAll(String.fromCharCode(8206), '') // Left-To-Right Mark
+      .replaceAll(String.fromCharCode(8234), '') // Left-To-Right Embedding
+      .trim();
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      words: response,
+    }),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
 };
 
 export { handler };
