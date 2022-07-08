06100f0db7f5310999270c937425770a5cb7815d
Piero Giusti <pierophp@gmail.com>
2022-07-08 14:02:49 -0300

    Fix path

--- services/jieba/netlify/functions/separateWords.ts
+++ services/jieba/netlify/functions/separateWords.ts
@@ -29,9 +29,11 @@ const handler: Handler = async (event, context) => {
 
   const response = separateChineseWords(body.text, {
     dictPath:
-      process.env.NODE_PATH + '/separate-chinese-words/dict/jieba.dict.utf8',
+      process.env.PWD +
+      '/node_modules/separate-chinese-words/dict/jieba.dict.utf8',
     userDictPath:
-      process.env.NODE_PATH + '/separate-chinese-words/dict/user.dict.utf8',
+      process.env.PWD +
+      '/node_modules/separate-chinese-words/dict/user.dict.utf8',
   }).filter((item) => {
     return item
       .replaceAll(String.fromCharCode(160), '') // Convert NO-BREAK SPACE to Space
