0c85fa481a8e39eee5d9c550c09bc4a9ad97f9c5
Piero Giusti <pierophp@gmail.com>
2022-07-08 12:17:09 -0300

    Fix .gitignore

---
+++ services/jieba/netlify/functions/separateWords.ts
@@ -0,0 +1,38 @@
+import { Handler } from '@netlify/functions';
+import { separateChineseWords } from 'separate-chinese-words';
+
+const handler: Handler = async (event, context) => {
+  const body = event.body ? JSON.parse(event.body) : null;
+
+  if (!body?.text) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ message: 'Text is required' }),
+    };
+  }
+
+  const response = separateChineseWords(body.text, {
+    dictPath: 'node_modules/separate-chinese-words/dict/jieba.dict.utf8',
+    userDictPath: 'node_modules/separate-chinese-words/dict/user.dict.utf8',
+  }).filter((item) => {
+    return item
+      .replaceAll(String.fromCharCode(160), '') // Convert NO-BREAK SPACE to SPACE
+      .replaceAll(String.fromCharCode(8201), '') // Convert THIN SPACE to SPACE
+      .replaceAll(String.fromCharCode(8203), '') // Zero Width Space
+      .replaceAll(String.fromCharCode(8206), '') // Left-To-Right Mark
+      .replaceAll(String.fromCharCode(8234), '') // Left-To-Right Embedding
+      .trim();
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      words: response,
+    }),
+    headers: {
+      'content-type': 'application/json',
+    },
+  };
+};
+
+export { handler };
