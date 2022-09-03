553d226b54380e767834ad74993b481dfc2cce1a
Piero Giusti <pierophp@gmail.com>
2022-09-03 17:14:43 -0300

    Files first commit

--- services/files/src/index.ts
+++ services/files/src/index.ts
@@ -1,30 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { S3 } from '@aws-sdk/client-s3';
+import { S3Adapter } from 'node-filesystem/adapters/s3.adapter';
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-	// MY_BUCKET: R2Bucket;
+  AWS_REGION: string;
+  AWS_S3_ENDPOINT: string;
+  AWS_S3_BUCKET: string;
+  AWS_ACCESS_KEY_ID: string;
+  AWS_SECRET_ACCESS_KEY: string;
 }
 
 export default {
-	async fetch(
-		request: Request,
-		env: Env,
-		ctx: ExecutionContext
-	): Promise<Response> {
-		return new Response("Hello World!");
-	},
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
+    const s3Client = new S3({
+      region: env.AWS_REGION,
+      endpoint: env.AWS_S3_ENDPOINT,
+      credentials: {
+        accessKeyId: env.AWS_ACCESS_KEY_ID!,
+        secretAccessKey: env.AWS_SECRET_ACCESS_KEY!,
+      },
+    });
+
+    const fileAdapter = new S3Adapter(s3Client, env.AWS_S3_BUCKET, '/');
+
+    // await fileAdapter.write('1/teste.txt', 'Teste');
+
+    return Response.json({
+      test: await fileAdapter.listContents('/1'),
+    });
+  },
 };
