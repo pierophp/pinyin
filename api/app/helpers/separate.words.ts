99a0a44f88779b48336a81723f6234d30dd3eef1
Piero Giusti <pierophp@gmail.com>
2024-07-15 19:57:06 -0300

    Separte Wirds

--- api/app/helpers/separate.words.ts
+++ api/app/helpers/separate.words.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+
 export async function separateWords(text: string): Promise<string[]> {
   const jiebaBaseUrl =
     process.env.JIEBA_URL ?? 'https://pinyin-jieba.netlify.app';
