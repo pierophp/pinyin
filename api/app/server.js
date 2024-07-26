8eead0093499357361ee88b6c6c029a9cc8d94c5
Piero Giusti <pierophp@gmail.com>
2024-07-26 17:34:59 -0300

    node js

--- api/app/server.js
+++ api/app/server.js
@@ -2,7 +2,6 @@ require('ts-node/register');
 const app = require('./index');
 
 const port = 'PORT' in process.env ? process.env.PORT : 9090;
-process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = 0;
 
 app.listen(port, () => {
   // eslint-disable-next-line no-console
