f7612ce095adc73cbe6829cdfdf710bd63dae020
Piero Giusti <pierophp@gmail.com>
2024-07-26 21:58:49 -0300

    User agent

--- api/app/core/sites/downloader.ts
+++ api/app/core/sites/downloader.ts
@@ -5,7 +5,7 @@ export class Downloader {
     const response = await http.get(url, {
       headers: {
         'user-agent':
-          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
       },
     });
 
