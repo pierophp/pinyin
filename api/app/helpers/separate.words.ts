7ed82a19419077bb834af0138bbfc0d5de0851f2
Piero Giusti <pierophp@gmail.com>
2024-07-26 22:40:47 -0300

    Node Jieba

--- api/app/helpers/separate.words.ts
+++ api/app/helpers/separate.words.ts
@@ -1,54 +1,60 @@
-import axios from 'axios';
+// import axios from 'axios';
 
+// export async function separateWords(text: string): Promise<string[]> {
+//   const jiebaBaseUrl =
+//     process.env.JIEBA_URL ?? 'https://pinyin-jieba.netlify.app';
 
-export async function separateWords(text: string): Promise<string[]> {
-  const jiebaBaseUrl =
-    process.env.JIEBA_URL ?? 'https://pinyin-jieba.netlify.app';
+//   const response = (
+//     await axios.post(`${jiebaBaseUrl}/.netlify/functions/separateWords`, {
+//       text,
+//     })
+//   ).data.words;
 
-  const response = (
-    await axios.post(`${jiebaBaseUrl}/.netlify/functions/separateWords`, {
-      text,
-    })
-  ).data.words;
+//   return response;
+// }
 
-  return response;
+import * as nodejieba from 'nodejieba';
+import * as replaceall from 'replaceall';
+
+let jiebaInstance;
+
+function getJiebaInstance(): any {
+  if (jiebaInstance) {
+    return jiebaInstance;
+  }
+
+  let dictPath = `${__dirname.replace(
+    'dist/api/',
+    '',
+  )}/../data/jieba.full.utf8`;
+
+  let userDictPath = `${__dirname.replace(
+    'dist/api/',
+    '',
+  )}/../data/compiled.utf8`;
+
+  console.log({ dictPath, userDictPath });
+
+  nodejieba.load({
+    dict: dictPath,
+    userDict: userDictPath,
+  });
+
+  jiebaInstance = nodejieba;
+
+  return jiebaInstance;
 }
 
-// import * as nodejieba from 'nodejieba';
-// import * as replaceall from 'replaceall';
+export function separateWords(text: string): string[] {
+  return getJiebaInstance()
+    .cut(text)
+    .filter((item) => {
+      item = replaceall(String.fromCharCode(160), '', item); // Convert NO-BREAK SPACE to SPACE
+      item = replaceall(String.fromCharCode(8201), '', item); // Convert THIN SPACE to SPACE
+      item = replaceall(String.fromCharCode(8203), '', item); // Zero Width Space
+      item = replaceall(String.fromCharCode(8206), '', item); // Left-To-Right Mark
+      item = replaceall(String.fromCharCode(8234), '', item); // Left-To-Right Embedding
 
-// let jiebaInstance;
-
-// function getJiebaInstance(): any {
-//   if (jiebaInstance) {
-//     return jiebaInstance;
-//   }
-
-//   let dictPath = `${__dirname.replace('dist/api/', '')}/../data/jieba.full.utf8`;
-//   let userDictPath = `${__dirname.replace('dist/api/', '')}/../data/compiled.utf8`
-
-//   console.log({dictPath, userDictPath });
-
-//   nodejieba.load({
-//     dict: dictPath,
-//     userDict: userDictPath,
-//   });
-
-//   jiebaInstance = nodejieba;
-
-//   return jiebaInstance;
-// }
-
-// export function separateWords(text: string): string[] {
-//   return getJiebaInstance()
-//     .cut(text)
-//     .filter(item => {
-//       item = replaceall(String.fromCharCode(160), '', item); // Convert NO-BREAK SPACE to SPACE
-//       item = replaceall(String.fromCharCode(8201), '', item); // Convert THIN SPACE to SPACE
-//       item = replaceall(String.fromCharCode(8203), '', item); // Zero Width Space
-//       item = replaceall(String.fromCharCode(8206), '', item); // Left-To-Right Mark
-//       item = replaceall(String.fromCharCode(8234), '', item); // Left-To-Right Embedding
-
-//       return item.trim();
-//     });
-// }
+      return item.trim();
+    });
+}
